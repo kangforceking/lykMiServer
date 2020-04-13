39c8440bb991bde5ddf01e1dbcd6364bd3a44c78
luyongkang <2726411438@qq.com>
2020-04-13 18:58:18 +0800

    使用koa-router

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const koa = require('koa') 
 const app = new koa()
-const server = require('http').Server(app.callback())
-const io = require('socket.io')(server);
+// const server = require('http').Server(app.callback())
+// const io = require('socket.io')(server);
 const port = 3000;
 
-server.listen(port, () => {
+const router = require('./src/router/routers')
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(3001)
+
+app.listen(port, () => {
     console.log(`app run at : http://127.0.0.1:${port}`);
 })
 
-io.on('connection', socket => {
-    console.log('socket已连接');
-    socket.on('msg', (data) => {
-        console.log(data)
-        socket.emit('backMsg', data)
-    })
-    socket.on('disconnect', function () {
-        console.log('已断开')
-    })
-})
+// io.on('connection', socket => {
+//     console.log('socket已连接');
+//     socket.on('msg', (data) => {
+//         console.log(data)
+//         socket.emit('backMsg', data)
+//     })
+//     socket.on('disconnect', function () {
+//         console.log('已断开')
+//     })
+// })
